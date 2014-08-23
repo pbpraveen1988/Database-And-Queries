f3a1e0547d244f4bd1aa7c9d1a4ff5abf1e47833
pbpraveen1988 <pbpraveen1988@gmail.com>
2014-08-23 16:44:52 +0530

    Connect With FluentNhibernate

---
+++ Queries.cs
@@ -0,0 +1,399 @@
+﻿using NHibernate.Criterion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Linq;
+
+namespace DatabaseAndQueries
+{
+    public class Queries
+    {
+
+        /// <summary>
+        /// Inserts The Data Into Table Related To Class
+        /// </summary>
+        public static void Add<T>(T newobject)
+        {
+            using (NHibernate.ISession session = SessionFactory.GetNewSession())
+            {
+                using (NHibernate.ITransaction transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        session.Save(newobject);
+
+                        transaction.Commit();
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// BulkInsertion Will Be Done On The Table Related To The Class
+        /// </summary>
+        public static void BulkInsert<T>(List<T> objList)
+        {
+            using (NHibernate.IStatelessSession session = SessionFactory.GetNewStateLessSession())
+            {
+                using (NHibernate.ITransaction transaction = session.BeginTransaction())
+                {
+
+                    try
+                    {
+                        int count = objList.Count();
+                        session.SetBatchSize(count);
+
+                        foreach (var item in objList)
+                        {
+                            session.Insert(item);
+                        }
+                        transaction.Commit();
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retrieves All The Data From Table Related to the Class
+        /// </summary>
+        public static List<T> GetAllData<T>()
+        {
+            using (NHibernate.ISession session = SessionFactory.GetNewSession())
+            {
+                using (NHibernate.ITransaction transaction = session.BeginTransaction())
+                {
+
+                    try
+                    {
+                        return session.CreateCriteria(typeof(T)).List<T>().ToList<T>();
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retrieves All The Data From Table 
+        /// Related to the Class with Sorting Order
+        /// Provided at the Time Of Calling
+        /// </summary>
+        public static List<T> GetAllDataWithOrder<T, K>(Expression<Func<T, K>> Sortorder, bool OrderByDescending)
+        {
+            using (NHibernate.ISession session = SessionFactory.GetNewSession())
+            {
+                using (NHibernate.ITransaction transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        if (OrderByDescending)
+                        {
+
+                            return session.Query<T>().OrderByDescending(Sortorder).ToList<T>();
+                        }
+                        else
+                        {
+                            return session.Query<T>().OrderBy(Sortorder).ToList<T>();
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the Specified number of elements from the
+        /// Specified Start
+        /// </summary>
+        public static List<T> GetAllDataWithOrderAndCount<T, K>(Expression<Func<T, K>> SortOrder, bool OrderByDescending, int StartCount, int MaxResults)
+        {
+            using (NHibernate.ISession session = SessionFactory.GetNewSession())
+            {
+                using (NHibernate.ITransaction transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        if (OrderByDescending)
+                        {
+
+                            return session.Query<T>().OrderByDescending(SortOrder).Skip<T>(StartCount).Take<T>(MaxResults).ToList<T>();
+                        }
+                        else
+                        {
+                            return session.Query<T>().OrderBy(SortOrder).Skip<T>(StartCount).Take<T>(MaxResults).ToList<T>();
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the Specified number of elements from the
+        /// Specified Start with filteration 
+        /// </summary>
+        public static List<T> GetAllDataWithOrderConditionAndCount<T, K>(Expression<Func<T, bool>> Predicate, Expression<Func<T, K>> SortOrder, bool OrderByDescending, int StartCount, int MaxResults)
+        {
+            using (NHibernate.ISession session = SessionFactory.GetNewSession())
+            {
+                using (NHibernate.ITransaction transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+
+                        if (OrderByDescending)
+                        {
+
+                            return session.Query<T>().Where<T>(Predicate).OrderByDescending(SortOrder).Skip<T>(StartCount).Take<T>(MaxResults).ToList<T>();
+                        }
+                        else
+                        {
+                            return session.Query<T>().Where<T>(Predicate).OrderBy(SortOrder).Skip<T>(StartCount).Take<T>(MaxResults).ToList<T>();
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Retrieve the Data from Table Related to Class With Filteration
+        /// </summary>
+        public static T GetDataByCondition<T>(Expression<Func<T, bool>> predicate)
+        {
+            using (NHibernate.ISession session = SessionFactory.GetNewSession())
+            {
+                using (NHibernate.ITransaction transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        return session.Query<T>().Where(predicate).FirstOrDefault<T>();
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+
+
+
+        /// <summary>
+        /// Retrieve All Data from Table Related to Class With Filteration
+        /// </summary>
+        public static List<T> GetAllByCondition<T>(Expression<Func<T, bool>> predicate)
+        {
+            using (NHibernate.ISession session = SessionFactory.GetNewSession())
+            {
+                using (NHibernate.ITransaction transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        return session.Query<T>().Where(predicate).ToList<T>();
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Retrieve All Data from Table Related to Class
+        /// With Filteration with Sorting According to time of 
+        /// Calling
+        /// </summary>
+        public static List<T> GetAllByConditionWithOrder<T, K>(Expression<Func<T, bool>> predicate, Expression<Func<T, K>> OrderPredicate, bool OrderByDescending)
+        {
+            using (NHibernate.ISession session = SessionFactory.GetNewSession())
+            {
+                using (NHibernate.ITransaction transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        if (OrderByDescending)
+                        {
+                            return session.Query<T>().Where(predicate).OrderByDescending(OrderPredicate).ToList<T>();
+
+                        }
+                        else
+                        {
+                            return session.Query<T>().Where(predicate).OrderBy(OrderPredicate).ToList<T>();
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+            }
+
+        }
+
+
+        /// <summary>
+        /// Delete the Elements from the Database 
+        /// according to the Object
+        /// </summary>
+        public static void Delete<T>(T newObject)
+        {
+            using (NHibernate.ISession session = SessionFactory.GetNewSession())
+            {
+                using (NHibernate.ITransaction transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        session.Delete(newObject);
+                        transaction.Commit();
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Delete the Elements from the Database 
+        /// by Filteration
+        /// </summary>
+        public static int Delete<T>(Expression<Func<T, bool>> predicate)
+        {
+            using (NHibernate.ISession session = SessionFactory.GetNewSession())
+            {
+                using (NHibernate.ITransaction transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        List<T> obj = GetAllByCondition(predicate);
+                        foreach (var item in obj)
+                        {
+                            session.Delete(item);
+                        }
+                        transaction.Commit();
+                        return obj.Count;
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Check whether the provided Condition is Exists or Not in Database
+        /// </summary>
+        public static bool IsExists<T>(Expression<Func<T, bool>> Predicate)
+        {
+            using (NHibernate.ISession session = SessionFactory.GetNewSession())
+            {
+                using (NHibernate.ITransaction transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        List<T> _ListT = session.Query<T>().Where(Predicate).ToList<T>();
+                        if (_ListT != null)
+                        {
+                            if (_ListT.Count > 0)
+                                return true;
+                            else
+                                return false;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+            }
+        }
+
+
+
+        /// <summary>
+        /// Update the Particular Data in Table Related to the class
+        /// </summary>
+        public static void Update<A>(A newobject)
+        {
+            using (NHibernate.ISession session = SessionFactory.GetNewSession())
+            {
+                using (NHibernate.ITransaction transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        session.Update(newobject);
+                        transaction.Commit();
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Update the Particular Data in Table Related to the class
+        /// with Filteration
+        /// </summary>
+        public static void UpdateWithCondition<T>(Expression<Func<T, bool>> predicate)
+        {
+            using (NHibernate.ISession session = SessionFactory.GetNewSession())
+            {
+                using (NHibernate.ITransaction transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        List<T> _ListT = GetAllByCondition(predicate);
+                        foreach (var item in _ListT)
+                        {
+                            session.Update(item);
+
+                        }
+                        transaction.Commit();
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SessionFactory.cs
@@ -0,0 +1,160 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Conventions.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAndQueries
+{
+    public class SessionFactory
+    {
+        private static NHibernate.ISessionFactory sFactory;
+
+        public static string DatabaseName { get; set; }
+        public static string DatabaseHost { get; set; }
+        public static string DatabaseUser { get; set; }
+        public static string DatabasePassword { get; set; }
+        public static string DatabaseType { get; set; }
+        public static string AssemblyName { get; set; }
+
+        public SessionFactory(string DBName, string DBHost, string DBType, string DBUser, string DBPassword, string ProjectName)
+        {
+            DatabaseName = DBName;
+            DatabaseHost = DBHost;
+            DatabaseUser = DBUser;
+            DatabasePassword = DBPassword;
+            DatabaseType = DBType;
+            AssemblyName = ProjectName;
+        }
+
+        public static void Init()
+        {
+            try
+            {
+
+
+                if (DatabaseType == DBTYPE.MySql.ToString())
+                {
+                    var ConfigurationString = MySQLConfiguration.Standard.ConnectionString(c => c.Is("Data Source=" + DatabaseHost + ";User ID=" + DatabaseUser + ";Password=" + DatabasePassword + ";persist security info=False;initial catalog=" + DatabaseName + ";charset=utf8;"));
+                    sFactory = sFactory = Fluently.Configure().Database(ConfigurationString)
+                      .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.LoadWithPartialName(AssemblyName)))
+                      .BuildSessionFactory();
+
+                    //Fluently.Configure()
+                    //            .Database(ConfigurationString)
+                    //            .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.LoadWithPartialName(AssemblyName)).Conventions.Add(
+                    //             ForeignKey.Format((x, y) =>
+                    //             String.Concat(y.Name, "Id"))))
+                    //            .BuildSessionFactory();
+
+                }
+                else if (DatabaseType == DBTYPE.MsSql2005.ToString())
+                {
+                    var ConfigurationString = MsSqlConfiguration.MsSql2005.ConnectionString(c => c.Is("Data Source=" + DatabaseHost + ";User ID=" + DatabaseUser + ";Password=" + DatabasePassword + ";persist security info=False;initial catalog=" + DatabaseName + ";charset=utf8;"));
+                    sFactory = Fluently.Configure().Database(ConfigurationString)
+                      .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.LoadWithPartialName(AssemblyName)))
+                      .BuildSessionFactory();
+                }
+                else if (DatabaseType == DBTYPE.MsSql2008.ToString())
+                {
+                    var ConfigurationString = MsSqlConfiguration.MsSql2008.ConnectionString(c => c.Is("Data Source=" + DatabaseHost + ";User ID=" + DatabaseUser + ";Password=" + DatabasePassword + ";persist security info=False;initial catalog=" + DatabaseName + ";charset=utf8;"));
+                    sFactory = Fluently.Configure().Database(ConfigurationString)
+                      .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.LoadWithPartialName(AssemblyName)))
+                      .BuildSessionFactory();
+                }
+                else if (DatabaseType == DBTYPE.MsSql2012.ToString())
+                {
+                    var ConfigurationString = MsSqlConfiguration.MsSql2012.ConnectionString(c => c.Is("Data Source=" + DatabaseHost + ";User ID=" + DatabaseUser + ";Password=" + DatabasePassword + ";persist security info=False;initial catalog=" + DatabaseName + ";charset=utf8;"));
+                    sFactory = Fluently.Configure().Database(ConfigurationString)
+                      .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.LoadWithPartialName(AssemblyName)))
+                      .BuildSessionFactory();
+                }
+                else if (DatabaseType == DBTYPE.Oracle.ToString())
+                {
+
+                }
+                else if (DatabaseType == DBTYPE.PostGreSql.ToString())
+                {
+
+                }
+                else if (DatabaseType == DBTYPE.SqlLite.ToString())
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public static NHibernate.ISessionFactory GetSessionFactory()
+        {
+            if (sFactory == null)
+            {
+                if (!string.IsNullOrEmpty(DatabaseType))
+                {
+                    if (!string.IsNullOrEmpty(DatabaseHost))
+                    {
+                        if (!string.IsNullOrEmpty(DatabaseName))
+                        {
+                            if (!string.IsNullOrEmpty(DatabaseUser))
+                            {
+                                Init();
+                            }
+                            else
+                            {
+                                throw new Exception("Database UserName is not Defined");
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception("Database Name is not Defined");
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("Database Host is not Defined");
+                    }
+                }
+                else
+                {
+                    throw new Exception("Database Type is not Defined");
+                }
+            }
+            return sFactory;
+        }
+
+        public static NHibernate.ISession GetNewSession()
+        {
+            return GetSessionFactory().OpenSession();
+        }
+
+        public static NHibernate.IStatelessSession GetNewStateLessSession()
+        {
+            return GetSessionFactory().OpenStatelessSession();
+        }
+        public static void CloseSession()
+        {
+            GetSessionFactory().Close();
+            sFactory.Close();
+            sFactory = null;
+        }
+
+
+    }
+
+    public enum DBTYPE
+    {
+        MySql,
+        MsSql2005,
+        MsSql2008,
+        MsSql2012,
+        Oracle,
+        PostGreSql,
+        SqlLite
+    }
+}
